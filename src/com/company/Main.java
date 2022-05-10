c7ec7d6c205c15aa8f8cefdf824d42b1e779f0be
AgentG5 <maksum3000@gmail.com>
2022-05-10 23:30:22 +0300

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //zavdanja 1
+
+//        System.out.print(Math.round(Math.random() * 40 - 20));
+
+        //zavdanja 2
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Гіпотинуза : ");
+//        int lm = sc.nextInt();
+//
+//        Scanner sc_2 = new Scanner(System.in);
+//        System.out.println("перший катет  : ");
+//        int lm_2 = sc_2.nextInt();
+//
+//        Scanner sc_3 = new Scanner(System.in);
+//        System.out.println("другий катет : ");
+//        int lm_3 = sc_3.nextInt();
+//
+//        if(b == 3 && c == 4){
+//            System.out.println("P = " + (lm + lm_2 + lm_3) );
+//            System.out.println("S = " + (lm_2 + lm_3) /2 );
+//        }else System.out.println("не вірне значення !");
+
+
+        //zavdanja 3
+
+//        int [] number = new int[1];
+//        for(int i = 0; i <number.length; i++){
+//            number[i] =(int) Math.round(Math.random()*102);
+//            if(number[i] <= 9){
+//                System.out.println(number[i] + " Одна цифра");
+//            }else if(number[i] <= 99){
+//                System.out.println(number[i] + " Дві цифри");
+//            }else {
+//                System.out.println(number[i] + " Три цифри");
+//            }
+//        }
+
+
+
+        //zavdanja 4
+
+
+
+//        int value = 0;
+//        int[] num = new int[3];
+//
+//        for (int i = 0; i < num.length; i++) {
+//            num[i] = (int) Math.round(Math.random() * 2 + 1);
+//            System.out.println(num[i]);
+//        }
+//        int[] num_2 = new int[3];
+//        while (value != 2) {
+//            for (int a = 0; a < num_2.length; a++) {
+//                Scanner sc = new Scanner(System.in);
+//                System.out.println("Ведіть числа :");
+//                num_2[a] = sc.nextInt();
+//
+//            }
+//            value++;
+//            if (Arrays.equals(num, num_2)) {
+//                System.out.println("---- Вітаю ви виграли !! ----");
+//                break;
+//            }else if (value == 2){
+//                System.out.println("---- Повезе наступного разу ( :  ----");
+//            }else System.out.println("---- Спробуй ще раз ----");
+//
+//        }
+    }
+}
